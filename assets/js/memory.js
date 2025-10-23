9581a346870da0c3c8273e8a10e9c250cf12c35e
Lucca <alves.lucca@aluno.ifsp.edu.br>
2025-10-23 12:10:30 -0300

    Memory Game started. Count isn't working kms

--- assets/js/memory.js
+++ assets/js/memory.js
@@ -1,34 +1,47 @@
-const memoryGrid = document.querySelector("#memoryGrid");
-const maxCardCount = 2
-const images = []
+const memoryGrid = document.querySelector("#memoryGrid")
+let maxCardCount = 3
 
-const dolphinImage = {
-    src: "",
-    count: 0
+const cards = [
+    {
+        src: "../img/icons/memoryCards/dolphin.jpg",
+        count: 0
+    },
+    {
+        src: "../img/icons/memoryCards/kelp.jpg",
+        count: 0
+    },
+    {
+        src: "../img/icons/memoryCards/shark.png",
+        count: 0
+    },
+]
+
+let avaiableCards = cards.filter((card) => {
+    if (card.count < maxCardCount) {
+        return (card)
+    }
+})
+
+function addCards() {
+    for (let i = 0; i < (maxCardCount * cards.length); i++) {
+
+        const newCard = document.createElement("div")
+        let image = document.createElement("img")
+        let randomCard = avaiableCards[Math.floor(Math.random() * maxCardCount)]
+        randomCard.count++
+        image.src = randomCard.src
+        image.classList.add("cardImage")
+
+        newCard.appendChild(image)
+        memoryGrid.appendChild(newCard)
+
+        avaiableCards = avaiableCards.filter((card) => {
+            if (card.count < maxCardCount) {
+                console.log(card)
+                return (card)
+            }
+        })
+
+    }
 }
-images.push(dolphinImage)
-
-const turtleImage = {
-    src: "",
-    count: 0
-}
-images.push(turtleImage)
-
-const kelpImage = {
-    src: "",
-    count: 0
-}
-images.push(kelpImage)
-
-function randomImage() {
-    const rdmImage = Math.floor(Math.random() )
-}
-
-function newTile() {
-
-    const newTile = document.createElement("div");
-    const tileImage = document.createElement("img");
-    tileImage.src = 
-    newTile.appendChild(tileIMG);
-    memorygrid.appendChild(newTile)
-}
+addCards()
