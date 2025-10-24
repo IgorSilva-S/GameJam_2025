9c1acf64dfde8b4d8b0ae284d60f1778fdd11223
lucca <kelcoolg@gmail.com>
2025-10-23 21:39:07 -0300

    Memory game cards changed, flipping animation & card randomization implemented

--- assets/js/memory.js
+++ assets/js/memory.js
@@ -1,47 +1,60 @@
 const memoryGrid = document.querySelector("#memoryGrid")
 let maxCardCount = 3
 
-const cards = [
+let cards = [
     {
         src: "../img/icons/memoryCards/dolphin.jpg",
-        count: 0
+    },
+    {
+        src: "../img/icons/memoryCards/dolphin.jpg",
     },
     {
         src: "../img/icons/memoryCards/kelp.jpg",
-        count: 0
+    },
+    {
+        src: "../img/icons/memoryCards/kelp.jpg",
     },
     {
         src: "../img/icons/memoryCards/shark.png",
-        count: 0
+    },
+    {
+        src: "../img/icons/memoryCards/shark.png",
     },
 ]
 
-let avaiableCards = cards.filter((card) => {
-    if (card.count < maxCardCount) {
-        return (card)
+function shuffleArray(array){
+    for (let i = array.length - 1; i > 0; i--){
+        const random = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[random]] = [array[random], array[i]];
     }
-})
+}
 
 function addCards() {
-    for (let i = 0; i < (maxCardCount * cards.length); i++) {
-
+    shuffleArray(cards)
+    for (const card of cards) {
+        const cardContainer = document.createElement("div")
         const newCard = document.createElement("div")
-        let image = document.createElement("img")
-        let randomCard = avaiableCards[Math.floor(Math.random() * maxCardCount)]
-        randomCard.count++
-        image.src = randomCard.src
-        image.classList.add("cardImage")
-
-        newCard.appendChild(image)
-        memoryGrid.appendChild(newCard)
-
-        avaiableCards = avaiableCards.filter((card) => {
-            if (card.count < maxCardCount) {
-                console.log(card)
-                return (card)
-            }
+        const cardFront = document.createElement("div")
+        const cardBack = document.createElement("div")
+        const backImage = document.createElement("img")
+        backImage.src = card.src
+        backImage.classList = "backImage"
+        cardFront.classList = "front"
+        cardBack.classList = "back"
+        newCard.classList = "card"
+        cardContainer.classList = "cardContainer"
+        cardBack.appendChild(backImage)
+        newCard.appendChild(cardFront)
+        newCard.appendChild(cardBack)
+        cardContainer.appendChild(newCard)
+        memoryGrid.appendChild(cardContainer)
+        cardContainer.addEventListener('click', () => {
+            newCard.style.transform = "rotateY(180deg)"
+            setTimeout(() => {
+                newCard.style.transform = "rotateY(0deg)"
+            }, 2000);
         })
-
     }
 }
 addCards()
